7442157a55ec691ed8992cba40ffacbd8995d974
tls11 <theospielman@gmail.com>
2023-07-01 22:19:52 -0700

    upgradde

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
-const cards = fs.readFileSync('cards.txt')
+const cards = fs.readFileSync('./decks/kobold.txt')
   .toString()
   .split('\n')
   .map(entry => /\d{0,} {0,}(.*)/.exec(entry)[1]);
@@ -12,7 +12,7 @@ console.log('cards', cards);
   const driver = await new Builder().forBrowser('firefox').build();
   for (const card of cards) {
     await driver.get('https://www.cardkingdom.com');
-    await driver.findElement(By.id('tags')).sendKeys(card, Key.ENTER);
+    await driver.findElement(By.id('header-search-input')).sendKeys(card, Key.ENTER);
     await driver.switchTo().newWindow('tab');
   }
   driver.close();
